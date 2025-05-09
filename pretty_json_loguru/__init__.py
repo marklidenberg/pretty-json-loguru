8a9c85b7a9381dc64463e42faef43023d6592774
marklidenberg <marklidenber@gmail.com>
2025-05-09 13:30:38 +0200

    chore: change pyproject.toml description

--- pretty_json_loguru/__init__.py
+++ pretty_json_loguru/__init__.py
@@ -1,3 +1,10 @@
+from importlib.metadata import version
+
+try:
+    __version__ = version("dony")
+except Exception:
+    __version__ = "unknown"
+
 from .setup_json_loguru import setup_json_loguru
 
 from .pretty_json_loguru_formatter import pretty_json_loguru_formatter
